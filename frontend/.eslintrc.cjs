67f347d118215ef42a537e670bcb80b2ea17fc0e
KordyD <84672403+KordyD@users.noreply.github.com>
2024-02-23 13:28:26 +0300

    Init frontend

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh', 'unused-imports'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'no-console': 'warn',
+  },
+};
