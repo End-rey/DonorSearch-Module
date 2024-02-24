70decc01545541f6118c5f363eacd3fe17a39caf
End-rey <andreyis2017@gmail.com>
2024-02-24 09:18:48 +0300

    test

--- backend/main.py
+++ backend/main.py
@@ -1,94 +1,31 @@
-"""
-This example shows how to use webhook on behind of any reverse proxy (nginx, traefik, ingress etc.)
-"""
-import logging
-import sys
-from os import getenv
-
-from aiohttp import web
-
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message, InputFile
-from aiogram.utils.markdown import hbold
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-
-TOKEN = getenv("BOT_TOKEN")
-
-# Webserver settings
-# bind localhost only to prevent any external access
-WEB_SERVER_HOST = "127.0.0.1"
-# Port for incoming request from reverse proxy. Should be any available port
-WEB_SERVER_PORT = 8080
-
-# Path to webhook route, on which Telegram will send requests
-WEBHOOK_PATH = "/webhook"
-# Secret key to validate requests from Telegram (optional)
-WEBHOOK_SECRET = "my-secret"
-# Base URL for webhook will be used to generate webhook URL for Telegram,
-# in this example it is used public DNS with HTTPS support
-BASE_WEBHOOK_URL = "https://31.129.45.151/"
-
-certificate = InputFile(path="/etc/nginx/ssl/nginx2.pem")
-
-# All handlers should be attached to the Router (or Dispatcher)
-router = Router()
-
-@router.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+from fastapi import FastAPI
+from aiogram import types, Dispatcher, Bot
+from bot import dp, bot
+from config import TELEGRAM_BOT_TOKEN, NGROK_TUNNEL_URL
 
 
-@router.message()
-async def echo_handler(message: types.Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
+app = FastAPI()
+WEBHOOK_PATH = f"/bot/{TELEGRAM_BOT_TOKEN}"
+WEBHOOK_URL = f"{NGROK_TUNNEL_URL}{WEBHOOK_PATH}"
 
 
-async def on_startup(bot: Bot) -> None:
-    # If you have a self-signed SSL certificate, then you will need to send a public
-    # certificate to Telegram
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", 
-                          secret_token=WEBHOOK_SECRET, 
-                          certificate=certificate)
+@app.on_event("startup")
+async def on_startup():
+    webhook_info = await bot.get_webhook_info()
+    if webhook_info.url != WEBHOOK_URL:
+        await bot.set_webhook(
+            url=WEBHOOK_URL
+        )
 
 
-def main() -> None:
-    # Dispatcher is a root router
-    dp = Dispatcher()
-    # ... and all other routers should be attached to Dispatcher
-    dp.include_router(router)
-
-    # Register startup hook to initialize webhook
-    dp.startup.register(on_startup)
-
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-
-    # Create aiohttp.web.Application instance
-    app = web.Application()
-
-    # Create an instance of request handler,
-    # aiogram has few implementations for different cases of usage
-    # In this example we use SimpleRequestHandler which is designed to handle simple cases
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=WEBHOOK_SECRET,
-    )
-    # Register webhook handler on application
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    # Mount dispatcher startup and shutdown hooks to aiohttp application
-    setup_application(app, dp, bot=bot)
-
-    # And finally start webserver
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    telegram_update = types.Update(**update)
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    await dp.process_update(telegram_update)
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    main()
+@app.on_event("shutdown")
+async def on_shutdown():
+    await bot.session.close()
