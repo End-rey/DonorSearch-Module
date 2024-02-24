4aded30b63b7b4445a168ac99d7d69108a3c0c27
KordyD <84672403+KordyD@users.noreply.github.com>
2024-02-24 04:20:39 +0300

    Final prototype donation form

--- frontend/src/components/Donation/DonationPlace.tsx
+++ frontend/src/components/Donation/DonationPlace.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import RadioButtonCard from '@/components/CustomUI/RadioButtonCard';
+import { fieldProp } from '@/components/Donation/Donation';
+import {
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { RadioGroup } from '@radix-ui/react-radio-group';
 
-const DonationPlace = () => {
-  return <div>DonationPlace</div>;
+const DonationPlace = ({ field }: { field: fieldProp<'donationPlace'> }) => {
+  return (
+    <FormItem>
+      <FormLabel>Место сдачи</FormLabel>
+      <FormControl>
+        <RadioGroup
+          className='space-y-4'
+          onValueChange={field.onChange}
+          defaultValue={field.value}
+        >
+          <RadioButtonCard
+            header={'Стационарный пункт'}
+            text={
+              'Питание или компенсация питания бла бла бла бла или МРОТ и всё такое'
+            }
+            value='station'
+          />
+          <RadioButtonCard
+            header={'Выездная акция'}
+            text={
+              'Питание или компенсация питания бла бла бла бла или МРОТ и всё такое'
+            }
+            value='event'
+          />
+        </RadioGroup>
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  );
 };
 
 export default DonationPlace;
