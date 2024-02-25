dc80064a5b99d1f22953e4d68aae7d3e1786700c
Marina Lysyuk <lysyuk.m.o@gmail.com>
2024-02-25 07:11:24 +0300

    Update regions.sql
    fix regions

--- backend/data/regions.sql
+++ backend/data/regions.sql
@@ -1,31 +1,13 @@
-INSERT INTO region
-VALUES ('Москва', 0);
-INSERT INTO region
-VALUES ('Санкт-Петербург', 0);
-INSERT INTO region
-VALUES ('Московская обл.', 0);
-INSERT INTO region
-VALUES ('Ленинградская обл.', 0);
-INSERT INTO region
-VALUES ('Тульская обл.', 0);
-INSERT INTO region
-VALUES ('Волгоградская обл.', 0);
-INSERT INTO region
-VALUES ('Пензенская обл.', 0);
-INSERT INTO region
-VALUES ('Красноярский край', 0);
-INSERT INTO region
-VALUES ('Краснодарская обл.', 0);
-INSERT INTO region
-VALUES ('Приморский край', 0);
-INSERT INTO region
-VALUES ('Свердловская обл.', 0);
-INSERT INTO region
-VALUES ('Ростовская обл.', 0);
-INSERT INTO region
-VALUES ('Самарская обл.', 0);
-INSERT INTO region
-VALUES ('Брестская обл.', 1);
-INSERT INTO region
-VALUES ('Витебская обл.', 1);
+INSERT INTO region (name, country_id)
+VALUES ('Москва', 0),
+('Санкт-Петербург', 0),
+('Киевская обл.', 2),
+('Татарстан', 0),
+('Новосибирская обл.', 0),
+('Башкоторстан', 0),
+('Свердловская обл.', 0),
+('Нижегородская обл.', 0),
+('Минская обл.', 1),
+('Ярославская обл.', 0),
+('Омская обл.', 0);
 
